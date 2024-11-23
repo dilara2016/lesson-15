6e59c7813b6c4bb1a07e6407ba20bc9855b5480b
dilara2016 <m.123.hunain@gmail.com>
2024-11-23 10:22:37 +0100

    BREAK

---
+++ break.py
@@ -0,0 +1,11 @@
+#take user input
+a = input ("Enter a word: ")
+#program to check break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #condition 1
+     #display result
+     print("A is found")
+     break
+    else:
+        
+     print("A not found")
